8298edf6a6c7dc50dc75613556a706338a59dcbf
Lennon Grinta <lgrinta@gmail.com>
2017-12-28 12:52:24 -0800

    Deploy.java tasks and roles WIP

--- src/main/java/com/cj/adsystems/deployment/Deploy.java
+++ src/main/java/com/cj/adsystems/deployment/Deploy.java
@@ -3,10 +3,15 @@ package com.cj.adsystems.deployment;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.devicefarm.model.CPU;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.CreateVpcRequest;
 import com.amazonaws.services.ecr.AmazonECR;
 import com.amazonaws.services.ecr.AmazonECRClientBuilder;
 import com.amazonaws.services.ecr.model.AuthorizationData;
@@ -16,17 +21,14 @@ import com.amazonaws.services.ecr.model.GetAuthorizationTokenResult;
 import com.amazonaws.services.ecr.model.RepositoryAlreadyExistsException;
 import com.amazonaws.services.ecs.AmazonECS;
 import com.amazonaws.services.ecs.AmazonECSClientBuilder;
-import com.amazonaws.services.ecs.model.Cluster;
-import com.amazonaws.services.ecs.model.ContainerDefinition;
-import com.amazonaws.services.ecs.model.CreateClusterRequest;
-import com.amazonaws.services.ecs.model.LaunchType;
-import com.amazonaws.services.ecs.model.RegisterTaskDefinitionRequest;
+import com.amazonaws.services.ecs.model.*;
 import com.amazonaws.util.Base64;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.DockerCmdExecFactory;
 import com.github.dockerjava.api.model.AuthResponse;
 import com.github.dockerjava.api.model.BuildResponseItem;
 import com.github.dockerjava.api.model.Identifier;
+import com.github.dockerjava.api.model.Network;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
@@ -36,19 +38,7 @@ import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
 import com.google.common.collect.ImmutableSet;
 
 public class Deploy {
-	/* 
-	 * Configuration Options Follow.
-	 * These should be environment variables or args to main.
-	 */
-	public static final Regions AWS_REGION = Regions.US_EAST_1;
-	public static final String APPLICATION_NAME = "fargate-demo";
-	public static final String ENVIRONMENT_NAME = "lab";
-	public static final File DOCKER_WORKING_DIR = new File(".");
-	public static final String AWS_REGISTRY_ID="727586729164";
-	public static final String DOCKER_TAG = "deploy";
-	/* 
-	 * End Configuration Options
-	 */
+
 	
 	//public static final File SLASH_TARGET_SLASH_CLASSES = new File(Deploy.class.getClassLoader().getResource("thisistheroot").getPath());
 	//public static final File DOCKER_WORKING_DIR = SLASH_TARGET_SLASH_CLASSES.getParentFile().getParentFile().getParentFile();
@@ -56,30 +46,119 @@ public class Deploy {
 	private static final Logger logger = Logger.getLogger(Deploy.class.getName());
 
 	public static void main(String[] args) throws Exception {
+
+		/*
+		 * Configuration Options Follow.
+		 * These should be environment variables or args to main.
+		 */
+		Regions AWS_REGION = Regions.US_EAST_1;
+		String CLUSTER_NAME = "ad-systems";
+		String APPLICATION_NAME = "fargate-demo";
+		String ENVIRONMENT_NAME = "lab";
+		File DOCKER_WORKING_DIR = new File(".");
+		String AWS_REGISTRY_ID="727586729164";
+		String DOCKER_TAG = "deploy";
+		Integer MEMORY = 512;
+		Integer CPU = 256;
+		Set<String> VPC_SUBNETS=ImmutableSet.of("subnet-6f752a45", "subnet-361d1240", "subnet-14154d4c", "subnet-b64d718b", "subnet-d7c941db", "subnet-d5bdddb0");
+		Set<String> VPC_SECURITY_GROUPS = ImmutableSet.of("sg-a84f08d3");
+
+		/*
+		 * End Configuration Options
+		 */
+
+
 		AmazonECR ecr = AmazonECRClientBuilder.standard().withRegion(AWS_REGION).build();
 		AmazonECS ecs = AmazonECSClientBuilder.standard().withRegion(AWS_REGION).build();
+		AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withRegion(AWS_REGION).build();
 		//https://stackoverflow.com/questions/40099527/pulling-image-from-amazon-ecr-using-docker-java
-		createEcrRepository(ecr);		
+		createEcrRepository(ecr, APPLICATION_NAME);
 		DockerClient dockerClient = setupDocker(ecr);
-	    String ecrImageName = dockerBuild(dockerClient, AWS_REGISTRY_ID, AWS_REGION, APPLICATION_NAME, DOCKER_TAG);
+	    String ecrImageName = dockerBuild(dockerClient, DOCKER_WORKING_DIR, AWS_REGISTRY_ID, AWS_REGION, APPLICATION_NAME, DOCKER_TAG);
 		dockerPush(ecrImageName, dockerClient); 
 		
 		
 		//Here's where we begin translating the Fargate guide http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ECS_AWSCLI_Fargate.html
-		Cluster cluster = createCluster("ad-systems", ecs);
-		createTask(ecs);
+		Cluster cluster = createCluster(CLUSTER_NAME, ecs);
+		//NetworkConfiguration networkConfiguration = createNetworkConfiguration(ec2);
+		NetworkConfiguration networkConfiguration = new NetworkConfiguration().withAwsvpcConfiguration(new AwsVpcConfiguration().withSecurityGroups(VPC_SECURITY_GROUPS).withSubnets(VPC_SUBNETS));
+		run(ecs, cluster, networkConfiguration, AWS_REGISTRY_ID, ecrImageName, APPLICATION_NAME, MEMORY, CPU);
 		
 		
 		
 	    logger.info("Complete.");
 	}
 
+//	private static NetworkConfiguration createNetworkConfiguration(AmazonEC2 ec2) {
+//		ec2.createVpc(new CreateVpcRequest()..withCidrBlock("10.0.0.0/16")).getVpc();
+//	}
 
-	private static void createTask(AmazonECS ecs) {
-//		ContainerDefinition containerDefinition = new ContainerDefinition().wi;
-//		RegisterTaskDefinitionRequest request = new RegisterTaskDefinitionRequest().withContainerDefinitions(containerDefinitions)
-//		ecs.registerTaskDefinition(registerTaskDefinitionRequest);
-		
+
+	private static void run(AmazonECS ecs, Cluster cluster, NetworkConfiguration networkConfiguration, String accountId, String ecrImageName, String applicationName, Integer memory, Integer cpu) {
+		String roleARN = String.format("arn:aws:iam::%s:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS", accountId);
+		//String roleARN = String.format("arn:aws:iam::%s:role/nodes.k8s.dev.cjadsystems.com", accountId);
+
+		/*
+		The deploying user needs the following role:
+		    "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Resource": "*"
+
+            Otherwise aws ecs describe-services --cluster ad-systems --services fargate-demo
+            leads to error ECS was unable to assume the role
+		 */
+
+
+
+		ContainerDefinition containerDefinition = new ContainerDefinition()
+				.withImage(ecrImageName)
+				.withName(applicationName);
+				//.withMemory(memory);
+		RegisterTaskDefinitionRequest taskRequest = new RegisterTaskDefinitionRequest()
+				.withContainerDefinitions(ImmutableSet.of(containerDefinition))
+				.withFamily(applicationName)
+				.withCpu(cpu.toString())
+				.withRequiresCompatibilities(Compatibility.FARGATE)
+				.withNetworkMode(NetworkMode.Awsvpc)
+				.withMemory(memory.toString())
+				.withExecutionRoleArn(roleARN);
+		TaskDefinition task = ecs.registerTaskDefinition(taskRequest).getTaskDefinition();
+
+
+
+
+//		CreateClusterRequest createClusterRequest = new CreateClusterRequest().withClusterName("fargate-cluster");
+
+
+
+		CreateServiceRequest createServiceRequest = new CreateServiceRequest()
+				.withCluster(cluster.getClusterName())
+				.withServiceName(applicationName)
+				.withTaskDefinition(task.getTaskDefinitionArn())
+				.withDesiredCount(2)
+				.withLaunchType("FARGATE")
+				.withNetworkConfiguration(networkConfiguration);
+		try {
+			CreateServiceResult createServiceResult = ecs.createService(createServiceRequest);
+		}catch(Exception e){
+			//Assume that the service already exists.  Attempt to update it.
+			ecs.updateService(new UpdateServiceRequest()
+					.withCluster(cluster.getClusterName())
+					.withService(applicationName)
+					.withTaskDefinition(task.getTaskDefinitionArn())
+					.withDesiredCount(2)
+					.withNetworkConfiguration(networkConfiguration));
+		}
+
+
+		//aws ecs create-service
+		// -cluster fargate-cluster
+		// --service-name fargate-service
+		// --task-definition sample-fargate:1
+		// --desired-count 2
+		// --launch-type "FARGATE"
+		// --network-configuration "awsvpcConfiguration={subnets=[subnet-abcd1234],securityGroups=[sg-abcd1234]}"
+
 	}
 
 
@@ -95,7 +174,7 @@ public class Deploy {
 	}
 
 
-	private static String dockerBuild(DockerClient dockerClient, String awsAccountNumber, Regions region, String applicationName, String dockerTag) {
+	private static String dockerBuild(DockerClient dockerClient, File workingFolder, String awsAccountNumber, Regions region, String applicationName, String dockerTag) {
 		//final String DOCKER_IMAGE_TAG = "727586729164.dkr.ecr."+AWS_REGION.getName()+".amazonaws.com/fargate-demo:"+GIT_COMMIT;
 		String ecrImageName = String.format("%s.dkr.ecr.%s.amazonaws.com/%s:%s", awsAccountNumber, region.getName(), applicationName, dockerTag);
 		BuildImageResultCallback callback = new BuildImageResultCallback() {
@@ -108,7 +187,7 @@ public class Deploy {
 	        }
 	    };
 	    
-	    dockerClient.buildImageCmd(DOCKER_WORKING_DIR).withTags(ImmutableSet.of(ecrImageName)).exec(callback).awaitImageId();
+	    dockerClient.buildImageCmd(workingFolder).withTags(ImmutableSet.of(ecrImageName)).exec(callback).awaitImageId();
 	    return ecrImageName;
 	}
 
@@ -142,13 +221,13 @@ public class Deploy {
 	}
 
 
-	private static void createEcrRepository(AmazonECR ecr) {
+	private static void createEcrRepository(AmazonECR ecr, String applicationName) {
 		CreateRepositoryRequest repoRequest = new CreateRepositoryRequest();
-		repoRequest.setRepositoryName(APPLICATION_NAME);
+		repoRequest.setRepositoryName(applicationName);
 		try {
 			ecr.createRepository(repoRequest);
 		}catch(RepositoryAlreadyExistsException e) {
-			logger.info(String.format("Registry %s Already Exists.", APPLICATION_NAME));
+			logger.info(String.format("Registry %s Already Exists.", applicationName));
 		}
 	}
 	
